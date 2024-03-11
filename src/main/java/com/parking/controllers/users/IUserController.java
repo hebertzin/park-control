d5e0b8ec058e191d291f76834a687e381911f969
hebertsanto <hebertsantos0704@gmail.com>
2024-03-11 19:48:06 -0300

    docs: add docs in vehicles controller and user

--- src/main/java/com/parking/controllers/users/IUserController.java
+++ src/main/java/com/parking/controllers/users/IUserController.java
@@ -2,13 +2,29 @@ package com.parking.controllers.users;
 
 import com.parking.domain.users.Users;
 import com.parking.domain.users.UsersDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 
 public interface IUserController {
 
+    @Operation(summary = "Cadastra um usuário no sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com seucesso"),
+            @ApiResponse(responseCode = "400", description = "Se houve algum erro", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+    })
     ResponseEntity<Users> createUser(UsersDTO user) throws Exception;
 
+    @Operation(summary = "Pega dados de um usuário no sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Consulta com seucesso"),
+            @ApiResponse(responseCode = "400", description = "Se houve algum erro", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+    })
     ResponseEntity<Users> findById(String id) throws EmptyResultDataAccessException;
 
 }
--- src/main/java/com/parking/controllers/vehicle/IVehicleController.java
+++ src/main/java/com/parking/controllers/vehicle/IVehicleController.java
@@ -2,15 +2,37 @@ package com.parking.controllers.vehicle;
 
 import com.parking.domain.vehicle.Vehicle;
 import com.parking.domain.vehicle.VehicleDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface IVehicleController {
 
+   @Operation(summary = "Adiciona um veiculo no sistema")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "201", description = "Veiculo adicionado com seucesso"),
+           @ApiResponse(responseCode = "400", description = "Se houve algum erro", content = @Content),
+           @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+   })
    ResponseEntity<Vehicle> addVehicle(VehicleDTO vehicle) throws Exception;
 
+   @Operation(summary = "Pega dados de um veiculo no sistema")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "Consulta com seucesso"),
+           @ApiResponse(responseCode = "400", description = "Se houve algum erro", content = @Content),
+           @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+   })
    ResponseEntity<Vehicle> listVehicleById(String id) throws Exception;
 
+   @Operation(summary = "APaga veiculo do sistema")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "deletado com seucesso"),
+           @ApiResponse(responseCode = "400", description = "Se houve algum erro", content = @Content),
+           @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+   })
    ResponseEntity<List<Vehicle>> getAllVehiclesByUserId(String id) throws Exception;
 }
