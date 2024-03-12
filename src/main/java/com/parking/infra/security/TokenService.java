b34341c79d8353150afa59e027cdc779d2b9f2ec
hebertsanto <hebertsantos0704@gmail.com>
2024-03-12 12:52:12 -0300

    feat: create a method to validate token

--- src/main/java/com/parking/infra/security/TokenService.java
+++ src/main/java/com/parking/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.parking.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.parking.domain.users.Users;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,17 +13,37 @@ public class TokenService {
 
     @Value("${spring.api.security.token.secret}")
     private String secret;
+
     public String generateToken(Users user){
       try {
+
           Algorithm algorithm = Algorithm.HMAC256(secret);
+
           String token = JWT.create()
                   .withIssuer("user-authentication")
                   .withSubject(user.getEmail())
                   .sign(algorithm);
 
           return  token;
+
       }catch (JWTCreationException exception){
           throw  new RuntimeException("Error creating token");
       }
+    }
+
+    public String validateToken(String token){
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("user-authentication")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTVerificationException exception){
+            return "";
+        }
     }
 }
