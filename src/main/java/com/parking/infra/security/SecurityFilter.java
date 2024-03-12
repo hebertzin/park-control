3fa3d40918e23c880c75ea09bad5e8f1a3f98d1d
hebertsanto <hebertsantos0704@gmail.com>
2024-03-12 13:06:58 -0300

    feat: add filter to jwt

--- src/main/java/com/parking/infra/security/SecurityFilter.java
+++ src/main/java/com/parking/infra/security/SecurityFilter.java
@@ -1,7 +1,46 @@
 package com.parking.infra.security;
 
+import com.parking.repository.users.UsersRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import java.io.IOException;
 
 @Component
-public class SecurityFilter {
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsersRepository user;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null){
+            var login = this.tokenService.validateToken(token);
+            UserDetails user = this.user.findByLogin(login);
+
+            var atuthenthication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(atuthenthication);
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+
 }
--- src/main/java/com/parking/repository/users/UsersRepository.java
+++ src/main/java/com/parking/repository/users/UsersRepository.java
@@ -1,8 +1,10 @@
 package com.parking.repository.users;
 import com.parking.domain.users.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends MongoRepository<Users, String> {
+    UserDetails findByLogin(String login);
 }
